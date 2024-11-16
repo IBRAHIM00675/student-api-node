52f16f695e25eae5f096207378de197970f31375
ibrahim musa <miharbiibra675@gmail.com>
2024-11-16 14:16:04 +0300

    jwtHelper.js

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -9,7 +9,7 @@ module.exports = {
             const secret = process.env.ACCESS_TOKEN_SECRET;
             const options = {
                 expiresIn: "10m",
-                issuer: "njauTechnologies.com",
+                issuer: "IbraTechnologies.com",
                 audience: UserId,
             }
             JWT.sign(payload, secret, options, (error, token)=>{
@@ -21,7 +21,7 @@ module.exports = {
 
     // middlware to verify accesstoken
     verifyAccessToken: (req, res, next)=>{
-        if(!req.headers["authoriztion"]) return next(createError.Unauthorized());
+        if(!req.headers["authorization"]) return next(createError.Unauthorized());
         const authHeader = req.headers['authorization'];
         const bearerToken = authHeader.split(' ');
         const token = bearerToken[1];
@@ -32,6 +32,37 @@ module.exports = {
             req.payload = payload;
             next()
       })
+    },
+
+
+    signRefreshToken:(UserId)=>{
+        return new Promise((resolve, reject)=>{
+            const payload ={}
+            const secret = process.env.REFRESH_TOKEN_SECRET;
+            const options ={
+                expiresIn: '1y',
+                issuer: 'IbraTechnologies.com',
+                audience: UserId,
+            }
+            JWT.sign(payload, secret, options, (error, token)=>{
+                if(error) {
+                    console.log(error.message)
+                    reject(createError.InternalServerError());
+                }  
+                resolve(token);
+            })
+        })
+    },
+
+    verifyRefreshToken: (refreshToken)=>{
+        return new Promise((resolve, reject)=>{
+            JWT.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, payload)=>{
+                if(err) return reject(createError.Unauthorized())
+                const userId = payload.aud
+
+                resolve(userId)
+            })
+        })
     }
 }
 
